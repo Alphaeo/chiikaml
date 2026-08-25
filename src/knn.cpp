e9147e6aa849e1a3349fce4dbad3447fd455e7be
Alphaeo <louis.mathis2002@gmail.com>
2026-08-25 08:08:07 +0200

    Implement KNNClassifier: brute-force k-NN with squared-distance ranking
    fit() stores training data (with dimension check), predict()/predict_one()
    find the k nearest neighbours via nth_element and vote by label majority.
    All 8 test cases / 18 assertions pass.

--- src/knn.cpp
+++ src/knn.cpp
@@ -1,6 +1,8 @@
 #include "chiikaml/knn.hpp"
 
 #include <stdexcept>
+#include <unordered_map>
+#include <algorithm>
 
 namespace chiikaml {
 
@@ -10,27 +12,41 @@ namespace chiikaml {
 // (0, 0) = une matrice vide, en attendant fit().
 KNNClassifier::KNNClassifier(std::size_t k)
     : k_(k), X_train_(0, 0) {
-    throw std::logic_error("KNNClassifier::KNNClassifier pas encore implemente");
 }
 
 // TODO(toi):
 // - verifie que X.rows() == y.size() (sinon throw std::invalid_argument)
 // - stocke X dans X_train_ et y (deplace ou copie) dans y_train_
 void KNNClassifier::fit(const Matrix& X, std::vector<int> y) {
-    throw std::logic_error("KNNClassifier::fit pas encore implemente");
+    
+    if (X.rows() != y.size()) {
+        throw std::invalid_argument("X.rows() must be equal to y.size()");
+    }
+    X_train_ = X;
+    y_train_ = std::move(y);
 }
 
 // TODO(toi): pour chaque ligne de X, appelle predict_one et empile
 // le resultat dans un vector<int> a retourner.
 std::vector<int> KNNClassifier::predict(const Matrix& X) const {
-    throw std::logic_error("KNNClassifier::predict pas encore implemente");
+    std::vector<int> predictions;
+    predictions.reserve(X.rows());
+    for (std::size_t i=0; i<X.rows(); ++i) {
+        predictions.push_back(predict_one(X,i));
+    }
+    return predictions;
 }
 
 // TODO(toi): somme des carres des differences entre
 // query(query_row, f) et X_train_(train_row, f) pour chaque feature f.
 double KNNClassifier::squared_distance(const Matrix& query, std::size_t query_row,
                                         std::size_t train_row) const {
-    throw std::logic_error("KNNClassifier::squared_distance pas encore implemente");
+    double squared_distance = 0;
+    for (std::size_t f=0; f<X_train_.cols(); ++f){
+        double diff = query(query_row, f) - X_train_(train_row, f);
+        squared_distance += diff * diff;
+    }
+    return squared_distance;
 }
 
 // TODO(toi):
@@ -38,7 +54,33 @@ double KNNClassifier::squared_distance(const Matrix& query, std::size_t query_ro
 // - trouve les k plus proches (regarde std::partial_sort ou std::nth_element)
 // - vote a la majorite parmi leurs labels (regarde std::unordered_map<int, int>)
 int KNNClassifier::predict_one(const Matrix& query, std::size_t query_row) const {
-    throw std::logic_error("KNNClassifier::predict_one pas encore implemente");
+    std::vector<std::pair<size_t, double>> distances;
+    for(std::size_t i=0; i<X_train_.rows(); ++i){
+        double dist = squared_distance(query, query_row, i);
+        distances.push_back({i, dist});
+    }
+    std::nth_element(distances.begin(), distances.begin() + k_, distances.end(),
+    [](const std::pair<std::size_t, double>& a, const std::pair<std::size_t, double>& b){
+        return a.second<b.second;
+    });
+
+    std::unordered_map<int, int> label_count;
+    for (std::size_t i=0; i<k_; ++i){
+        int label = y_train_[distances[i].first];
+        label_count[label]++;
+    }
+
+    int majority_label = -1;
+    int max_count = 0;
+
+    for (const auto& pair : label_count){
+        if (pair.second>max_count){
+            max_count = pair.second;
+            majority_label = pair.first;
+        }
+    }
+
+    return majority_label; ;
 }
 
 } // namespace chiikaml
